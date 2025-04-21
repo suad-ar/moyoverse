c249cfe410bbab3a1e3f31384f68f603a1e94c4a
Suad AR <suadrahman@live.com>
2025-04-21 17:53:52 +0000

    Plausible fixes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,15 +3,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* Cloudflare Web Analytics snippet */}
-        {/*
-        <script
-          defer
-          src="https://static.cloudflareinsights.com/beacon.min.js"
-          data-cf-beacon='{"token": "00000000-0000-0000-0000-000000000000"}'
-        ></script>
-        }
-        {/* Plausible Analytics */}
         <script
           async
           defer
